763e95e174d931af4c020c9a6168fc04b696a025
evanfrawley <evanfrawley@gmail.com>
2016-03-06 13:38:56 -0800

    adding more to readme and making script.r have proper url formatting

--- README.md
+++ README.md
@@ -14,3 +14,24 @@ API key: "AIzaSyAFot4QVTGFxQTMZB8_hUxIzOqQQ-WnxQI"
 example call that works!
 
 https://www.googleapis.com/youtube/v3/search?part=snippet&location=47.6550%2C-122.3080&locationRadius=5km&maxResults=5&q=meme&type=video&key=AIzaSyAFot4QVTGFxQTMZB8_hUxIzOqQQ-WnxQI
+https://www.googleapis.com/youtube/v3/videos?part=snippet&location=29.7604%2-95.3698&locationRadius=5km&maxResults=20&q=meme+memez&fields=items&key=AIzaSyAFot4QVTGFxQTMZB8_hUxIzOqQQ-WnxQI
+
+
+### Shiny APP functions
+
+- radius
+- radius unit ("km" or "mi" in radio buttons)
+- num of results
+- search items
+- toggle between searching/clicking and most popular videos
+  - panels / nav bar type thing?
+
+
+
+### MAP functions
+##### leaflet / mapbox
+
+- clickable points on the map to see link, details about the video
+- potenially showing charts of the X num of videos that are in that area
+- preview clips?
+- clicking on the maps gets a geolocation pointer, sends to the UI for input$location
--- script.R
+++ script.R
@@ -1,6 +1,3 @@
-
-
-
 #this cna be a place to put whatever scripts / test things that we may need to include in our porject
 
 library(jsonlite)
@@ -8,23 +5,32 @@ library(shiny)
 library(dplyr)
 library(leaflet)
 library(plotly)
+library(stringr)
 
+#youtube api dev key
 key <- "AIzaSyAFot4QVTGFxQTMZB8_hUxIzOqQQ-WnxQI"
 
+#base url for youtube api
 base_url <- "https://www.googleapis.com/youtube/v3/videos"
 
-location <- "29.7604,-95.3698"
+#geolocation given in a csv format
+location <- "29.7604,-95.3698" #csv's
 
+#radius of the circle aroudn the location
 radius <- "5km"
 
-search <- "q"
+#the search that the user wants
+search <- "meme memez" %>% 
+  str_replace(" ", "+")
 
+#number of results that are given
+n <- 20 
 
-
-url <- paste0("https://www.googleapis.com/youtube/v3/videos?part=snippet", 
-              "&chart=mostRecent",
+url <- paste0("https://www.googleapis.com/youtube/v3/search?part=snippet", 
               "&location=", location,
               "&locationRadius=", radius,
-              "&maxResults=1",
-              "&fields=items", "&key=", key)
-  
+              "&maxResults=", n,
+              "&q=", search,
+              "&type=video",
+              "&fields=items",
+              "&key=", key)
