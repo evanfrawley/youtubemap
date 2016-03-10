ed1985f8b193eb2cebc39d4ee7f2b5781b927595
Julie <engjulie@uw.edu>
2016-03-09 23:47:52 -0800

    update barchart server

--- test-barcharts/server.R
+++ test-barcharts/server.R
@@ -2,32 +2,22 @@ library(shiny)
 library(dplyr)
 library(plotly)
 
-#source('bar_graph.R')
 data <- read.csv(file = "sample.csv", TRUE, sep = ",")
 shinyServer(function(input, output) {
-  # Reactive expression to compose a data frame containing 
-  # certain information
-  datasetInput <- reactive({
-    switch(input$checkGroup,
-           1 == views,
-           2 == likes,
-           3 == dislikes)
-  })
   
   # Output bargraph 
   output$bargraph <- renderPlotly({
-    #data <- read.csv(file = "sample.csv", TRUE, sep = ",")
     plot_ly(data, 
             x = head(video,input$bins), 
-            y = datasetInput(), 
+            y = views, 
             type = "bar", 
             marker = list(color = 'orange'),
             name = "Views") %>% 
       layout(title = "Statistics on top videos in region",
              xaxis = list(title = "Youtube Videos"),
-             yaxis = list(title = "Count")) %>%
-      add_trace(y = datasetInput(), marker = list(color = 'green'), name = "Likes")
-    #input$checkGroup
+             yaxis = list(title = "Count"))  %>%
+      add_trace(y = likes, marker = list(color = 'green'), name = "Likes") %>% 
+      add_trace(y = dislikes, marker = list(color = 'red'), name = "Dislikes") 
   })
   
   
