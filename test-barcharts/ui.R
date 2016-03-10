bedce01fe7699be6721fa008d46503de5b4138fb
Julie <engjulie@uw.edu>
2016-03-09 23:49:13 -0800

    update barchart ui

--- test-barcharts/ui.R
+++ test-barcharts/ui.R
@@ -8,14 +8,8 @@ shinyUI(fluidPage(
     # Put sidebar widgets here
     sidebarPanel(
       helpText("These side bar widgets will be used to view statistics on the videos"),
-      # Widget 1: Checkbox group to see selected statistics on bar graph
-      checkboxGroupInput("checkGroup", "Check any of the following to view statistics", 
-                         choices = list("Views" = 1, 
-                                     "Likes" = 2, 
-                                     "Dislikes" = 3),
-                         selected = 1),
-      br(),
-      # Widget 2: Number of videos the user wants to show
+      
+      # Widget 1: Number of videos the user wants to show
       sliderInput("bins", "Number of videos you'd like to see statistics on:", 
                   min = 1, max = 50, value = 25, step = 1)
     ),
