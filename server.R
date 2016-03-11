f559ae691b0737aaaeaf1e13aff2e89e1c2d4448
Mary Tong <askmarytong@gmail.com>
2016-03-10 16:45:59 -0800

    new UI

--- server.R
+++ server.R
@@ -1,5 +1,16 @@
-#server.R
+server <- function(input, output) {
+  output$progressBox <- renderValueBox({
+    valueBox(
+      paste0(25 + input$count, "%"), "Progress", icon = icon("list"),
+      color = "purple"
+    )
+  })
+  
+  output$approvalBox <- renderValueBox({
+    valueBox(
+      "80%", "Approval", icon = icon("thumbs-up", lib = "glyphicon"),
+      color = "yellow"
+    )
+  })
+}
 
-library(shiny)
-library(dplyr)
-library(jsonlite)
--- ui.R
+++ ui.R
@@ -1,45 +1,71 @@
+library(shinydashboard)
 library(shiny)
 
-shinyUI(fluidPage(
-  includeCSS("styles.css"),
-  includeHTML("index.html"),
+# Widgets to allow users to input their search query, select the amount of results
+# shown, select the distance in radius and mi/km 
+sidebar <- dashboardSidebar( 
 
-   
-    sidebarPanel(
-      
-      # Widge 1: Search
-      textInput("search", label = h4("Search ID"),
-                value = "Keywords..."),
-      
-      # Widget 2: Input Radius
-      textInput("radius", label = h4("Input Radius"),
-                value = "Input Radius..."),
-      
-      br(),
-      # Widget 3: Amount of results 
-      sliderInput("bins", 
-                  label = "Number amount of results listed", 
-                  min = 1, max = 50, value = 15),
-
-      # Widget 4: Radio Button for miles or kilometers 
-      radioButtons("units", 
-                  label =  "Select miles or kilometers ", 
-                  choices =c("Miles"= "mi", "Kilometers" = "km"),
-                  selected = "Miles")
-
-      ),
-    # Main Panel
-    mainPanel(
-      
-      br(),
-      # Panel Tabset
-      tabsetPanel(type="tab", 
-                  tabPanel("Map", plotOutput("you_map"))
-
-      )
-      
-    )
+      # Widget #1: Search
+    sidebarSearchForm(textId = "Search", buttonId = "search",
+                      label = "Search..."),
     
+    # Widget #2: Select amount of results 
+    sliderInput("bins", "Amount of Results Display", 1, 50, 25),
+
+    br(),
+    
+    # Widget #3: Select the radius
+    sliderInput("radius", "Select the radius", 1, 50, 25),
+    
+    br(), 
+    
+    # Widget #4: Select miles or kilometers
+    radioButtons("units", "Select miles or kilometers", 
+                 choices = c("Miles" = "mi", "Kilometers" = "km"),
+                 selected = "Miles")
+  )
+
+# Tabs to select map, bar chart or list of results. Shows statistics like views, 
+# likes and dislikes 
+  body <- dashboardBody(
+    tags$head(tags$style(HTML('
+          .main-header .logo {
+                              font-family: "Rancho";
+                              font-style: normal;
+                              font-weight: 400;
+                              src: local("Rancho"), url(https://fonts.gstatic.com/s/rancho/v6/yJONObwbYmkcfKME0p1HqevvDin1pK8aKteLpeZ5c0A.woff2) format("woff2");
+                              unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2212, U+2215, U+E0FF, U+EFFD, U+F000;
+                              }
+                              .font-effect-shadow-multiple {
+                              text-shadow: .04em .04em 0 #fff,.08em .08em 0 #aaa;
+                              -webkit-text-shadow: .04em .04em 0 #fff, .08em .08em 0 #aaa;
+                              }
+                
+                              }
+                              '))),
+    
+    fluidRow(
+      tabBox(
+        id = "tabset1", width = "100px", height = "500px",
+        tabPanel("Interactive Map"),
+        tabPanel("Bar Chart"),
+        tabPanel("List of Results")
+      ),
+      
+      # Views statistic 
+      infoBox("Views", icon = icon("television"), color = 'teal', fill = TRUE),
+      
+      # Likes statistic
+      infoBox("Likes", icon = icon("thumbs-up"), color = 'red', fill = TRUE),
+      
+      # Dislikes
+      infoBox("Dislikes", icon = icon("thumbs-down"), fill = TRUE)
+      
+  )
+  )
   
-  ) 
-)
+  dashboardPage(skin = "black",
+    dashboardHeader(title = "YOUTUBE MAP"),
+    sidebar,
+    body
+  )
